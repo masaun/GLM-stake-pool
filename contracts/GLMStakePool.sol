609f3a3ab98972f63dfa6a32a30da9d36a0998ed
masaun <masanoriuno@gmail.com>
2020-12-19 22:49:28 +0900

    Integrate IUniswapV2Router01 into IUniswapV2Router02

--- contracts/GLMStakePool.sol
+++ contracts/GLMStakePool.sol
@@ -1,10 +1,22 @@
 pragma solidity ^0.5.10;
 pragma experimental ABIEncoderV2;
 
+/// Openzeppelin v2.5.1
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+
+/// Golem
 import { NewGolemNetworkToken } from "./golem/GNT2/NewGolemNetworkToken.sol";
+
+/// Uniswap-v2
+import { IUniswapV2Factory } from "./uniswap-v2/uniswap-v2-core/interfaces/IUniswapV2Factory.sol";
+import { IUniswapV2Pair } from "./uniswap-v2/uniswap-v2-core/interfaces/IUniswapV2Pair.sol";
+import { IUniswapV2ERC20 } from "./uniswap-v2/uniswap-v2-core/interfaces/IUniswapV2ERC20.sol";
+import { IUniswapV2Router02 } from "./uniswap-v2/uniswap-v2-periphery/interfaces/IUniswapV2Router02.sol";
 
 
 contract GLMStakePool {
+    using SafeMath for uint;
 
     NewGolemNetworkToken public GLMToken;
 
@@ -13,7 +25,7 @@ contract GLMStakePool {
     }
 
     /***
-     * @notice - Create a pair (LP token) between the GLM tokens and another ERC20 tokens
+     * @notice - Create a pair (LP token) between the GLM tokens and another ERC20 tokens by using Uniswap-V2
      *         - e.g). GLM/ETH, GLM/DAI, GLM/USDC
      **/
     function createLPToken() public returns (bool) {
