1d4649ea76f99f78b7bb176083f7c8860cb34e19
masaun <masanoriuno@gmail.com>
2020-12-19 11:15:06 +0900

    [Success to deploy on local]: Define migration file for GLM token (NewGolemNetworkToken.sol)

---
+++ migrations/2_deploy_golem-GLM.js
@@ -0,0 +1,15 @@
+const NewGolemNetworkToken = artifacts.require("NewGolemNetworkToken");
+
+require('dotenv').config();
+
+/// Get chain ID on the local
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'));
+const _networkId = web3.eth.net.getId();
+
+const _migrationAgent = process.env.WALLET_ADDRESS_2;
+const _chainId = web3.eth.getChainId();
+
+module.exports = function(deployer) {
+    deployer.deploy(NewGolemNetworkToken, _migrationAgent, _chainId);
+};
