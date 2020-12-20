d8f6aee057585b6db727c3c60c35a0fb8ae3cc6a
masaun <masanoriuno@gmail.com>
2020-12-21 08:57:26 +0900

    Define addLiquidityWithETH()

--- contracts/GLMStakePool.sol
+++ contracts/GLMStakePool.sol
@@ -9,7 +9,7 @@ import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
 import { NewGolemNetworkToken } from "./golem/GNT2/NewGolemNetworkToken.sol";
 
 /// WETH
-import { WETH } from "./uniswap-v2/uniswap-v2-periphery/interfaces/IWETH.sol";
+import { IWETH } from "./uniswap-v2/uniswap-v2-periphery/interfaces/IWETH.sol";
 
 /// Uniswap-v2
 import { UniswapV2Library } from "./uniswap-v2/uniswap-v2-periphery/libraries/UniswapV2Library.sol";
@@ -23,7 +23,7 @@ contract GLMStakePool {
     using SafeMath for uint;
 
     NewGolemNetworkToken public GLMToken;
-    WETH public wETH;
+    IWETH public wETH;
     IUniswapV2Factory public uniswapV2Factory;
     IUniswapV2Router02 public uniswapV2Router02;
 
@@ -32,7 +32,7 @@ contract GLMStakePool {
     address UNISWAP_V2_FACTORY;
     address UNISWAP_V2_ROUTOR_02;
 
-    constructor(NewGolemNetworkToken _GLMToken, WETH _wETH, IUniswapV2Factory _uniswapV2Factory, IUniswapV2Router02 _uniswapV2Router02) public {
+    constructor(NewGolemNetworkToken _GLMToken, IWETH _wETH, IUniswapV2Factory _uniswapV2Factory, IUniswapV2Router02 _uniswapV2Router02) public {
         GLMToken = _GLMToken;
         wETH = _wETH;
         uniswapV2Factory = _uniswapV2Factory;
@@ -137,48 +137,47 @@ contract GLMStakePool {
      *         - e.g). GLM/ETH
      **/
     function addLiquidityWithETH(
-        IERC20 erc20,
-        uint GLMTokenAmountDesired,
+        uint GLMTokenAmountDesired
     ) public payable returns (bool) {
         /// Transfer GLM tokens and ETH from a user
         GLMToken.transferFrom(msg.sender, address(this), GLMTokenAmountDesired);
-        uint amountETHFromUser = msg.value;
+        uint ETHAmountDesired = msg.value;
 
         /// [Todo]: Convert ETH (msg.value) to WETH (ERC20)
         wETH.deposit();
 
-
-
-
-
-        /// Check whether a pair contract exists or not
-        address pairAddress = uniswapV2Factory.getPair(GLM_TOKEN, address(erc20)); 
-        require (pairAddress > address(0), "This pair contract has not existed yet");
-
-        /// Check whether liquidity of a pair contract is enough or not
-        IUniswapV2Pair pair = IUniswapV2Pair(UniswapV2Library.pairFor(UNISWAP_V2_FACTORY, GLM_TOKEN, address(erc20)));
-        uint totalSupply = pair.totalSupply();
-        require (totalSupply > 0, "This pair's totalSupply is still 0. Please add liquidity at first");        
-
         /// Approve each tokens for UniswapV2Routor02
         GLMToken.approve(UNISWAP_V2_ROUTOR_02, GLMTokenAmountDesired);
-        erc20.approve(UNISWAP_V2_ROUTOR_02, ERC20AmountDesired);
 
         /// Add liquidity and pair
         uint GLMTokenAmount;
-        uint ERC20Amount;
+        uint ETHAmount;
         uint liquidity;
-        (GLMTokenAmount, ERC20Amount, liquidity) = _addLiquidityWithERC20(erc20,
-                                                                          GLMTokenAmountDesired,
-                                                                          ERC20AmountDesired);        
+        (GLMTokenAmount, ETHAmount, liquidity) = _addLiquidityWithETH(GLMTokenAmountDesired, ETHAmountDesired);        
     }
 
-    function _addLiquidityWithERC20(   /// [Note]: This internal method is added for avoiding "Stack too deep" 
-        IERC20 erc20,
+    function _addLiquidityWithETH(   /// [Note]: This internal method is added for avoiding "Stack too deep" 
         uint GLMTokenAmountDesired,
-        uint ERC20AmountDesired
-    ) internal returns (uint _GLMTokenAmount, uint _ERC20Amount, uint _liquidity) {
+        uint ETHAmountDesired
+    ) internal returns (uint _GLMTokenAmount, uint _ETHAmount, uint _liquidity) {
+        uint GLMTokenAmount;
+        uint ETHAmount;
+        uint liquidity;
 
+        /// Define each minimum amounts (range of slippage)
+        uint GLMTokenMin = GLMTokenAmountDesired.sub(1 * 1e18);  /// Slippage is allowed until -1 GLM desired
+        uint ETHAmountMin = ETHAmountDesired.sub(1 * 1e18);      /// Slippage is allowed until -1 DAI desired 
+
+        address to = msg.sender;
+        uint deadline = now.add(15 seconds);
+        (GLMTokenAmount, ETHAmount, liquidity) = uniswapV2Router02.addLiquidityETH(GLM_TOKEN,
+                                                                                   GLMTokenAmountDesired,
+                                                                                   GLMTokenMin,
+                                                                                   ETHAmountMin,
+                                                                                   to,
+                                                                                   deadline);
+
+        return (GLMTokenAmount, ETHAmount, liquidity);
     }
 
 }
