6896fc3a68947c31dd4741a38fb07baec3a8e724
masaun <masanoriuno@gmail.com>
2020-12-20 09:10:48 +0900

    Define createPairWithERC20()

--- contracts/GLMStakePool.sol
+++ contracts/GLMStakePool.sol
@@ -10,26 +10,52 @@ import { NewGolemNetworkToken } from "./golem/GNT2/NewGolemNetworkToken.sol";
 
 /// Uniswap-v2
 import { IUniswapV2Factory } from "./uniswap-v2/uniswap-v2-core/interfaces/IUniswapV2Factory.sol";
+import { IUniswapV2Router02 } from "./uniswap-v2/uniswap-v2-periphery/interfaces/IUniswapV2Router02.sol";
 import { IUniswapV2Pair } from "./uniswap-v2/uniswap-v2-core/interfaces/IUniswapV2Pair.sol";
 import { IUniswapV2ERC20 } from "./uniswap-v2/uniswap-v2-core/interfaces/IUniswapV2ERC20.sol";
-import { IUniswapV2Router02 } from "./uniswap-v2/uniswap-v2-periphery/interfaces/IUniswapV2Router02.sol";
 
 
 contract GLMStakePool {
     using SafeMath for uint;
 
     NewGolemNetworkToken public GLMToken;
+    IUniswapV2Factory public uniswapV2Factory;
+    IUniswapV2Router02 public uniswapV2Router02;
 
-    constructor(NewGolemNetworkToken _GLMToken) public {
+    address GLM_TOKEN;
+
+    constructor(NewGolemNetworkToken _GLMToken, IUniswapV2Factory _uniswapV2Factory, IUniswapV2Router02 _uniswapV2Router02) public {
         GLMToken = _GLMToken;
+        uniswapV2Factory = _uniswapV2Factory;
+        uniswapV2Router02 = _uniswapV2Router02;
+
+        GLM_TOKEN = address(_GLMToken);
+    }
+
+
+    /***
+     * @notice - Create a pair (LP token) between the GLM tokens and another ERC20 tokens
+     *         - e.g). GLM/DAI, GLM/USDC
+     * @param erc20 - e.g). DAI, USDC, etc...
+     **/
+    function createPairWithERC20(IERC20 erc20) public returns (IUniswapV2Pair pair) {
+        address pair = uniswapV2Factory.createPair(GLM_TOKEN, address(erc20)); 
+        return IUniswapV2Pair(pair);
     }
 
     /***
-     * @notice - Create a pair (LP token) between the GLM tokens and another ERC20 tokens by using Uniswap-V2
+     * @notice - Create a pair (LP token) between the GLM tokens and ETH (GLM/ETH)
+     **/
+    function createPairWithETH() public returns (bool) {}
+
+
+    /***
+     * @notice - Add Liquidity for a pair (LP token) between the GLM tokens and another ERC20 tokens by using Uniswap-V2
      *         - e.g). GLM/ETH, GLM/DAI, GLM/USDC
      **/
-    function createLPToken() public returns (bool) {
+    function addLiquidity() public returns (bool) {
         /// Add liquidity and pair
-    }
+    }    
+
 
 }
