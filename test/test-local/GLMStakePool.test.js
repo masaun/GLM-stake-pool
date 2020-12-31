02d710db83f6364a561b6cb634a6d98fb87bcac2
masaun <masanoriuno@gmail.com>
2020-12-31 17:40:01 +0900

    Assign value into constructor of GLMStakePool.test.js

--- test/test-local/GLMStakePool.test.js
+++ test/test-local/GLMStakePool.test.js
@@ -5,12 +5,14 @@ const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'
 /// Artifact of the GLMStakePool contract 
 const GLMStakePool = artifacts.require("GLMStakePool");
 const GLMMockToken = artifacts.require("GLMMockToken");
+const GolemFarmingLPToken = artifacts.require("GolemFarmingLPToken");
 const GolemGovernanceToken = artifacts.require("GolemGovernanceToken");
 
 /// Global variable
 let glmStakePool;
 let glmToken;
-let glmGovernanceToken;
+let golemFarmingLPToken;
+let golemGovernanceToken;
 
 /***
  * @dev - Execution COMMAND: $ truffle test ./test/test-local/GLMStakePool.test.js
@@ -26,19 +28,28 @@ contract("GLMStakePool", function(accounts) {
             glmToken = await GLMMockToken.new({ from: accounts[0] });
         });
 
-        it("Setup GLMMockToken contract instance", async () => {
-            glmGovernanceToken = await GolemGovernanceToken.new({ from: accounts[0] });
+        it("Setup GolemFarmingLPToken contract instance", async () => {
+            golemFarmingLPToken = await GolemFarmingLPToken.new({ from: accounts[0] });
         });
 
-        // it("Setup GLMStakePool contract instance", async () => {
-        //     const _oceanFarmingToken = oceanFarmingToken.address;
-        //     const _oceanGovernanceToken = oceanGovernanceToken.address;
-        //     const _oceanGovernanceTokenPerBlock = 1000;
-        //     const _startBlock = 0;
-        //     const _endBlock = 1000;
+        it("Setup GolemGovernanceToken contract instance", async () => {
+            golemGovernanceToken = await GolemGovernanceToken.new({ from: accounts[0] });
+        });
 
-        //     glmStakePool = await GLMStakePool.new({ from: accounts[0] });
-        // });
+        it("Setup GLMStakePool contract instance", async () => {
+            const _glmToken = glmToken.address;
+            const _golemFarmingLPToken = golemFarmingLPToken.address;
+            const _golemGovernanceToken = golemGovernanceToken.address;
+            const _uniswapV2Factory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";   /// [Note]: common contract address on mainnet and testnet
+            const _uniswapV2Router02 = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";  /// [Note]: common contract address on mainnet and testnet
+
+            glmStakePool = await GLMStakePool.new(_glmToken, 
+                                                  _golemFarmingLPToken, 
+                                                  _golemGovernanceToken, 
+                                                  _uniswapV2Factory, 
+                                                  _uniswapV2Router02,
+                                                  { from: accounts[0] });
+        });
     });
 
     describe("GLMStakePool", () => {
