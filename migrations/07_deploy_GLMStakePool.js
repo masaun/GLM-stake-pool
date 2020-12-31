847949b3125b36bd4498a0ef6b5148f5b9c9ec21
masaun <masanoriuno@gmail.com>
2020-12-31 17:21:05 +0900

    Fix migration file of GolemFarmingLPToken.sol (<=Rename from GLMPoolToken)

--- migrations/07_deploy_GLMStakePool.js
+++ migrations/07_deploy_GLMStakePool.js
@@ -1,6 +1,6 @@
 const GLMStakePool = artifacts.require("GLMStakePool");
 const GLMMockToken = artifacts.require("GLMMockToken");
-const GLMPoolToken = artifacts.require("GLMPoolToken");
+const GolemFarmingLPToken = artifacts.require("GolemFarmingLPToken");
 const GolemGovernanceToken = artifacts.require("GolemGovernanceToken");
 
 //@dev - Import from exported file
@@ -8,7 +8,7 @@ var contractAddressList = require('./addressesList/contractAddress/contractAddre
 var tokenAddressList = require('./addressesList/tokenAddress/tokenAddress.js');
 
 const _GLMToken = GLMMockToken.address;
-const _glmPoolToken = GLMPoolToken.address;
+const _golemFarmingLPToken = GolemFarmingLPToken.address;
 const _golemGovernanceToken = GolemGovernanceToken.address;
 const _uniswapV2Factory = contractAddressList["Ropsten"]["Uniswap"]["UniswapV2Factory"];
 const _uniswapV2Router02 = contractAddressList["Ropsten"]["Uniswap"]["UniswapV2Router02"];
@@ -17,7 +17,7 @@ const _uniswapV2Router02 = contractAddressList["Ropsten"]["Uniswap"]["UniswapV2R
 module.exports = async function(deployer, network, accounts) {
     await deployer.deploy(GLMStakePool, 
                           _GLMToken,
-                          _glmPoolToken, 
+                          _golemFarmingLPToken, 
                           _golemGovernanceToken,
                           _uniswapV2Factory, 
                           _uniswapV2Router02);
