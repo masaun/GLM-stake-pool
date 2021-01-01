dbca417494f6df6ff0c4593f293d7a0dd0d136d1
masaun <masanoriuno@gmail.com>
2021-01-01 21:14:03 +0900

    Add a parameter (IERC20) into getEstimatedETHforERC20()

--- contracts/UniswapV2Helper.sol
+++ contracts/UniswapV2Helper.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.5.16;
 pragma experimental ABIEncoderV2;
 
+/// Openzeppelin v2.5.1
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+
 /// WETH
 import { IWETH } from "./uniswap-v2/uniswap-v2-periphery/interfaces/IWETH.sol";
 
@@ -27,7 +31,7 @@ contract UniswapV2Helper {
     constructor(
         IUniswapV2Factory _uniswapV2Factory, 
         IUniswapV2Router02 _uniswapV2Router02
-    ) public returns (bool) {
+    ) public {
         uniswapV2Factory = _uniswapV2Factory;
         uniswapV2Router02 = _uniswapV2Router02;
         wETH = IWETH(uniswapV2Router02.WETH());
@@ -41,13 +45,14 @@ contract UniswapV2Helper {
         uint deadline = block.timestamp + 15; // using 'now' for convenience, for mainnet pass deadline from frontend!
         uniswapV2Router02.swapETHForExactTokens(msg.value)(erc20Amount, getPathForETHtoERC20(), address(this), deadline);
         
-        // refund leftover ETH to user
-        (bool success,) = msg.sender.call.value(address(this).balance)("");
+        /// refund leftover ETH to user
+        (bool success,) = msg.sender.call.value(address(this).balance)("");       /// [Note]: Solidity-v0.5
+        //(bool success,) = msg.sender.call{ value: address(this).balance }("");  /// [Note]: Solidity-v0.6
         require(success, "refund failed");
     }
   
-    function getEstimatedETHforERC20(uint erc20Amount) public view returns (uint[] memory) {
-        return uniswapV2Router02.getAmountsIn(erc20Amount, getPathForETHtoERC20());
+    function getEstimatedETHforERC20(IERC20 erc20, uint erc20Amount) public view returns (uint[] memory) {
+        return uniswapV2Router02.getAmountsIn(erc20Amount, getPathForETHtoERC20(erc20));
     }
 
     function getPathForETHtoERC20(IERC20 erc20) private view returns (address[] memory) {
@@ -62,6 +67,6 @@ contract UniswapV2Helper {
      * @notice - important to receive ETH
      **/
     function() payable external {}   /// [Note]: Solidity-v0.5
-    //receive() payable external {}  /// [Note]: Soldiity-v0.6.1 ~
+    //receive() payable external {}  /// [Note]: Soldiity-v0.6
 
 }
