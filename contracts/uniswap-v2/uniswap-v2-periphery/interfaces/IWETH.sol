901377fa05d3c4bd10be221c11cbeee85f0a8d28
masaun <masanoriuno@gmail.com>
2021-01-03 15:20:14 +0900

    Define approve() in the IWETH.sol

--- contracts/uniswap-v2/uniswap-v2-periphery/interfaces/IWETH.sol
+++ contracts/uniswap-v2/uniswap-v2-periphery/interfaces/IWETH.sol
@@ -2,6 +2,7 @@ pragma solidity >=0.5.0;
 
 interface IWETH {
     function deposit() external payable;
+    function approve(address spender, uint value) external returns (bool);
     function transfer(address to, uint value) external returns (bool);
     function withdraw(uint) external;
 }
